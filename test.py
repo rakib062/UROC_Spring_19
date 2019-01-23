afc5d44e2646b9110c55e004eb23a4e3483442f0
rakib <rakhasan@indiana.edu>
2019-01-22 23:28:44 -0500

    collecting replis

--- test.py
+++ test.py
@@ -7,3 +7,29 @@ print(len(tweets), ' tweets were collected')
 
 #save the tweets
 pickle.dump(tweets, open('filename','wb'))
+
+
+#example of collecting replis
+def tweepy_to_twarc(tweet):
+	'''Convert tweepy status object to a dictionary'''
+	tw = dict()
+	tw['id'] = tweet.id
+	tw['id_str'] = str(tweet.id)
+	tw['user'] = dict()
+	tw['user']['screen_name'] = tweet.user.screen_name
+	return tw
+
+twarc = Twarc(auth.ck, auth.cs, auth.ak, auth.acs)
+
+replies = []
+reps = twarc.replies(tweepy_to_twarc(tweet), recursive= True)
+rep = next(reps)
+while True:
+    try: 
+        rep = next(reps)
+        replies.append(rep)
+    except StopIteration:
+        break
+    except Exception as e:
+        print('error: ',e)
+ print(len(replies), ' replies collected')
